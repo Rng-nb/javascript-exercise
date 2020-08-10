c8674993454458c594c2e5341640c2f636f60460
Rng-nb <18782824515@163.com>
2020-08-10 15:24:23 +0800

    [yafeng Wang] feat: 1-string test pass

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,5 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const result = collection.filter(item => item.charAt(0) === '粤' && item.charAt(1) === 'A');
+  return result.length;
 }
