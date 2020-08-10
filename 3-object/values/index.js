fdb7a9e2682ecd3109ca82e8da7beec2f0a310e9
Rng-nb <18782824515@163.com>
2020-08-10 18:32:30 +0800

    [yafeng Wang] feat: 3-object pass

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,8 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let result = 0;
+  Object.values(source).forEach(function sum(string) {
+    result += Number.parseInt(string, 10);
+  });
+  return result;
 }
