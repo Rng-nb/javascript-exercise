2281672ba42d94f85337a9b1f7a0d972ab48b58e
Rng-nb <18782824515@163.com>
2020-08-10 16:17:34 +0800

    [yafeng Wang] feat: 2-array pass

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,16 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = collection.filter(item => item % 3 === 0);
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = [];
+  collection.forEach(item => {
+    if (!result.includes(item)) result.push(item);
+  });
+  return result;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,5 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const result = collection.map(item => String.fromCharCode(96 + item));
+  return result;
 }
